d1faf344846d3c1ddba95a581b7f10f97785c3ca
anjib.gogoi <sanjib.gogoi@timesinternet.in>
2018-01-25 23:52:50 +0530

    First project creation

---
+++ arwrnd/src/main/java/com/arw/arwrnd/App.java
@@ -0,0 +1,13 @@
+package com.arw.arwrnd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
